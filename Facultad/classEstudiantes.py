63234e1a43a9d6d1c1750ed10e42e9f656a4ae72
SoldaditoNaranja <anibaldiazangel@gmail.com>
2026-05-25 19:41:47 -0300

    Create classEstudiantes.py

---
+++ Facultad/classEstudiantes.py
@@ -0,0 +1,16 @@
+class Estudiantes():
+    def __init__(self, nombre, apellido, nroMat, carrera): #registro de estudiantes
+        self.nombre = nombre
+        self.apellido = apellido
+        self.nroMat = nroMat
+        self.carrera = carrera
+        self.inscriptosEn = []
+
+    def inscribirse(self):
+        print
+
+    def darseBaja(self):
+        print
+
+    def estadoEstCursos(self):
+        print

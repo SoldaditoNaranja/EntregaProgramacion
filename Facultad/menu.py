02951563a31174ab423ab24c4a1bcc8c8713e3d4
SoldaditoNaranja <anibaldiazangel@gmail.com>
2026-05-25 19:40:49 -0300

    Create menu.py

---
+++ Facultad/menu.py
@@ -0,0 +1,9 @@
+def mostrar_menu():
+
+    print("\n1- Agregar estudiante")
+    print("2- Agregar curso")
+    print("3- Inscribirse a un curso")
+    print("4- Darse de baja de un curso")
+    print("5- Consultar estado de estudiante")
+    print("6- Consultar estado de curso")
+    print("0- Salir")

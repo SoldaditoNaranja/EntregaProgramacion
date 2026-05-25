cb0026477309556d144bfc3f0b14c22741e815ea
SoldaditoNaranja <anibaldiazangel@gmail.com>
2026-05-25 19:42:07 -0300

    Create classCurso.py

---
+++ Facultad/classCurso.py
@@ -0,0 +1,18 @@
+class Curso():
+    def __init__(self, nomCurso, codCurso, profEncargado): #registro de cursos
+        self.nomCurso = nomCurso
+        self.codCurso = codCurso
+        self.profEncargado = profEncargado
+        self.estudiantesInscriptos = []
+
+    def agregarEst(self):
+        print
+
+    def eliminarEst(self):
+        print
+    
+    def hayCupos(self):
+        print
+
+    def estado(self):
+        print

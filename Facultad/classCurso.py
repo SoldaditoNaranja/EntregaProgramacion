5999262a3f18f186d1d4242df594cb0788b85669
SoldaditoNaranja <anibaldiazangel@gmail.com>
2026-05-25 23:03:06 -0300

    Update classCurso.py

--- Facultad/classCurso.py
+++ Facultad/classCurso.py
@@ -1,18 +1,24 @@
 class Curso():
-    def __init__(self, nomCurso, codCurso, profEncargado): #registro de cursos
-        self.nomCurso = nomCurso
-        self.codCurso = codCurso
-        self.profEncargado = profEncargado
+    def __init__(self, nomCurso, codCurso, profEncargado, capacidadMaxima): 
+        self.nomCurso = nomCurso.strip().title()
+        self.codCurso = codCurso.strip()
+        self.profEncargado = profEncargado.strip().title()
+        self.capacidadMaxima = int(capacidadMaxima)
         self.estudiantesInscriptos = []
 
-    def agregarEst(self):
-        print
+    def agregarEst(self, estudiante):
+        if self.hayCupos():
+            self.estudiantesInscriptos.append(estudiante)
+            print(f"Estudiante {estudiante.nombre} agregado al curso con éxito.")
+        else:
+            print("No se pudo agregar al estudiante porque el curso está lleno.")
 
-    def eliminarEst(self):
-        print
-    
+    def eliminarEst(self, estudiante):
+        if estudiante in self.estudiantesInscriptos:
+            self.estudiantesInscriptos.remove(estudiante)
+            print(f"Estudiante {estudiante.nombre} dado de baja de {self.nomCurso} con éxito.")
+        else:
+            print(f"El estudiante {estudiante.nombre} no estaba anotado en este curso.")
+
     def hayCupos(self):
-        print
-
-    def estado(self):
-        print
+        return len(self.estudiantesInscriptos) < self.capacidadMaxima

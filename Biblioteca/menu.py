89015106d315951976b70e20dfcfc9eae9baf771
SoldaditoNaranja <anibaldiazangel@gmail.com>
2026-05-25 23:31:29 -0300

    Update menu.py

--- Biblioteca/menu.py
+++ Biblioteca/menu.py
@@ -1,9 +1,9 @@
 def mostrar_menu():
 
-    print("\n1- Agregar miembro")
-    print("2- Agregar libro")
-    print("3- Prestar libro")
-    print("4- Devolver libro")
-    print("5- Consultar estado libro")
-    print("6- Consultar estado miembro")
-    print("0- Salir")
+    print("\n1- Agregar un Miembro")
+    print("2- Agregar un Libro")
+    print("3- Prestar un Libro")
+    print("4- Devolver un Libro")
+    print("5- Consultar estado Libro")
+    print("6- Consultar estado Miembro")
+    print("\n0- Salir")

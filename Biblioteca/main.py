6e17b26dd994ee344551c151995f9734e86a5330
joaquin1234555 <joacalandri3@gmail.com>
2026-05-26 00:04:56 -0300

    Add book lending functionality in main.py

--- Biblioteca/main.py
+++ Biblioteca/main.py
@@ -86,7 +86,18 @@ def main():
                         break
 
         
-
+        elif opcion == "3": #Prestar libro
+            miembro_encontrado = None
+            libro_encontrado = None
+            while True:
+                dni = input("Ingrese DNI del miembro: ")
+                for m in biblioteca.miembros:
+                    if m.dni == dni:
+                        miembro_encontrado = m
+                if miembro_encontrado:
+                    break
+                else:
+                    print("Ingrese un DNI valido")
                     
             while True:
                 titulo = input("Titulo del libro: ")
